6a73e13ebac78f38f38d77085f40d4a822fa32e0
shahzod7176 <qoshqulovshahzod9@gmail.com>
2024-10-11 17:05:36 +0500

    add

---
+++ root/drf_settings.py
@@ -0,0 +1,10 @@
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
