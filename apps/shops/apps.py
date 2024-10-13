9f2b8bd9011e80e3fda408366dfd65974db8677d
shahzod7176 <qoshqulovshahzod9@gmail.com>
2024-10-14 00:11:50 +0500

    add app

--- apps/shops/apps.py
+++ apps/shops/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class ShopsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.shops'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "shops"
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,16 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models import EmailField, BooleanField
 
-# Create your models here.
+from users.managers import CustomUserManager
+
+
+class User(AbstractUser):
+    username = None
+    email = EmailField(unique=True)
+    is_active = BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    objects = CustomUserManager()
+
+    # wishlist = ManyToManyField('shops.Book', CASCADE, blank=True)
